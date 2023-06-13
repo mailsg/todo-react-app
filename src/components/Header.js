dcbe4c0db8ae755a62bf68515333bc9a7f5e9ae2
Sandeep Ghosh <mailsg0912@gmail.com>
2023-06-13 09:44:35 +0530

    Todo App developed following the tutorial

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+const Header = () => (
+  <header style={
+              {
+                padding: '20px 0',
+                lineHeight: '1.4em',
+                color: '#aeadad',
+                textAlign: 'center',
+              }
+         }
+  >
+    <h1>Todos for today</h1>
+  </header>
+
+);
+export default Header;
